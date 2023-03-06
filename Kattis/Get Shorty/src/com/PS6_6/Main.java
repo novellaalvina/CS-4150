2a5afce1b6536ebe268d16fb6feef0af8df86a12
novellaalvina <112059794+novellaalvina@users.noreply.github.com>
2023-03-06 00:23:19 -0700

    Add files via upload

---
+++ Kattis/Get Shorty/src/com/PS6_6/Main.java
@@ -0,0 +1,129 @@
+package com.PS6_6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+
+    public static double dijkstra(HashMap<Integer, Node> cities, Node s, Node si)
+    {
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+
+        HashMap<Node, Double> alg = new HashMap<>();
+
+        for(Node n: cities.values())
+        {
+            alg.put(n, 0.0);
+        }
+
+        // set the 1st city distance to 0
+        alg.put(s, 1.0);
+
+        // put the source inside the queue
+        pq.add(s);
+
+        while (pq.size() != 0)
+        {
+            Node u = pq.poll();
+            for(double v : u.next_vertex){
+                double u_dist = alg.get(u);
+                double v_dist = alg.get(cities.get(v));
+//                double w = u.next_vertex.get(v);
+
+//                if (v_dist < u_dist * w)
+//                {
+//                    v_dist = u_dist * w;
+////                    alg.get(v.prev).prev = u;
+//                    alg.put(cities.get(v), v_dist);
+//                    pq.add(cities.get(v));
+//                }
+            }
+        }
+        return alg.get(si);
+    }
+    public static void main(String[] args) {
+
+        // vertex and edges
+        Scanner s = new Scanner(System.in);
+        int nm[] = new int[2];
+
+        int n = -1; // # vertex
+        int m = -1; // # edges
+
+        DecimalFormat df = new DecimalFormat("#.####"); // to format the decimal to 4 digits after .
+
+        while(true) {
+
+            HashMap<Integer, Node> vertex_info = new HashMap<Integer, Node>();
+            n = s.nextInt();
+            m = s.nextInt();
+            if(n == 0 && m == 0){
+                System.exit(0);
+                return;
+            }
+
+            // vertex
+            for (int i = 0; i < n; i++) {
+                vertex_info.put(i, new Node(i,0));
+            }
+
+            // edges
+            for (int i = 0; i < m; i++) {
+                int u = s.nextInt();
+                int v = s.nextInt();
+                double f = s.nextDouble();
+
+                Node u_tmp = vertex_info.get(u);
+                Node v_tmp = vertex_info.get(v);
+                u_tmp.next_vertex.add(v, f);
+                v_tmp.next_vertex.add(u, f);
+            }
+
+            // dijkstra
+            double shortest_path = -1;
+            shortest_path = dijkstra(vertex_info, vertex_info.get(0), vertex_info.get(n - 1));
+            System.out.printf("%.4f\n",shortest_path);
+        }
+
+    }
+
+}
+
+class Node implements Comparable<Node>
+{
+    public int v;
+//    public ArrayList<Node> next_vertex;
+    public ArrayList<Double> next_vertex;
+//    public double factor;
+    public double dist;
+
+    public Node(int v, double factor)
+    {
+        this.v = v;
+        this.next_vertex = new ArrayList<Double>();
+        this.dist = 0;
+    }
+
+    public int compareTo(Node n) {
+        if(this.dist < n.dist) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+//class Prev_dist
+//{
+//    public Node prev;
+//    public double dist;
+//
+//    public Prev_dist(Node prev, double dist)
+//    {
+//        this.prev = prev;
+//        this.dist = dist;
+//    }
+//}
+
---
+++ Kattis/Get Shorty/src/com/PS6_6/NewPS6.java
@@ -0,0 +1,116 @@
+package com.PS6_6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class NewPS6 {
+    public static double dijkstra(HashMap<Integer, ArrayList<Connection>> graph, int s, int d)
+    {
+        PriorityQueue<Connection> pq = new PriorityQueue<Connection>();
+
+        double[] alg = new double[d+1];
+
+//        for(int v: graph.keySet())
+//        {
+//            alg[v] = 0;
+//        }
+
+        alg[s] = 1;
+
+        pq.add(new Connection(s,1));
+
+        while (pq.size() != 0){
+            Connection u = pq.poll();
+            for (Connection v: graph.get(u.v)) {
+                double u_dist = alg[u.v];
+                double v_dist = alg[v.v];
+                double w = v.factor;
+
+                if (v_dist < u_dist * w) {
+                    v_dist = u_dist * w;
+                    alg[v.v]= v_dist;
+                    pq.add(v);
+                }
+            }
+        }
+        return alg[d];
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // vertex and edges
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        int nm[] = new int[2];
+
+        int n = -1; // # vertex
+        int m = -1; // # edges
+
+        DecimalFormat df = new DecimalFormat("#.####"); // to format the decimal to 4 digits after .
+
+        while(true) {
+
+            HashMap<Integer, ArrayList<Connection>> graph = new HashMap<>();
+            String[] s = bf.readLine().split(" ");
+            n = Integer.parseInt(s[0]);
+            m = Integer.parseInt(s[1]);
+            if(n == 0 && m == 0){
+                System.exit(0);
+                return;
+            }
+
+            // vertex
+            for (int i = 0; i < n; i++) {
+                ArrayList<Connection> neighbours = new ArrayList<>();
+                graph.put(i, neighbours);
+            }
+
+            // edges
+            for (int i = 0; i < m; i++) {
+                String[] s2 = bf.readLine().split(" ");
+                int u = Integer.parseInt(s2[0]);
+                int v = Integer.parseInt(s2[1]);
+                double f = Double.parseDouble(s2[2]);
+
+                graph.get(u).add(new Connection(v,f));
+                graph.get(v).add(new Connection(u,f));
+
+            }
+
+            // dijkstra
+            double shortest_path = -1;
+            shortest_path = dijkstra(graph, 0, n-1);
+            System.out.printf("%.4f\n",shortest_path);
+        }
+
+    }
+
+}
+
+class Connection implements Comparable<Connection>{
+    public int v;
+    public double factor;
+
+    public Connection(int v, double f){
+        this.v = v;
+        this.factor = f;
+    }
+
+    public int compareTo(Connection n) {
+        if(this.factor < n.factor) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+
+
+
+
